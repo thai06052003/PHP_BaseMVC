7fd1138d06690f3707d66e165f404cac513a73e3
thai06052003 <xuanthai0304@gmail.com>
2024-10-13 17:40:48 +0700

    update CLI create/delete Model

---
+++ app/controllers/Post.php
@@ -0,0 +1,13 @@
+<?php
+// Post Controller
+class Post extends Controller {
+    public $data = [], $model = [];
+
+    public function __construct(){
+        // construct
+    }
+    
+    public function index(){
+        // Index
+    }
+}
--- app/core/AppServiceProvider.php
+++ app/core/AppServiceProvider.php
@@ -1,8 +1,8 @@
 <?php
 class AppServiceProvider extends ServiceProvider {
     public function boot() {
-        $dataUsers = $this->db->table('users')->where('id', '=', 3)->first();
-        $data['userInfo'] = $dataUsers;
+        /* $dataUsers = $this->db->table('users')->where('id', '=', 3)->first();
+        $data['userInfo'] = $dataUsers; */
         $data['copyright'] = 'Copyright &copy; 2024 by XuanThai';
         View::share(($data));
     }
